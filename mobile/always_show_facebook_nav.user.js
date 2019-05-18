9ec570f1ebace87032a220cda43e662b3e5d0a39
Thomas Axelsson <thomasa88@gmail.com>
2019-05-18 11:21:07 +0200

    facebook-nav: Match touch.facebook.com as well

--- mobile/always_show_facebook_nav.user.js
+++ mobile/always_show_facebook_nav.user.js
@@ -2,6 +2,7 @@
 // @name Always show Facebook navigation bar
 // @namespace thomasa88
 // @match *://m.facebook.com/*
+// @match *://touch.facebook.com/*
 // @grant none
 // @license GNU GPL v3. Copyright Thomas Axelsson 2019
 // @version 1.0
